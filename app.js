1eb166017f7d346788d9b3161e958d91616b36af
Yussuf Elarif <Vemuez@gmail.com>
2016-09-13 11:49:18 +0100

    added necessary dependencies

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var sass = require('node-sass');
 var fs = require('fs');
 
+var express = require("express"); //gets module from node
+var app = express();
+var http = require('http'); //default node module bundled with node
+//var bodyParser = require('body-parser'); //parses body
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
+
 sass.render({
   file: 'www/sass/style.scss',
   outFile: 'www/css/style.min.css',
