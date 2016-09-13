c555b9760fd84b509fc1ed6e1dbb1327650204e7
Yussuf Elarif <Vemuez@gmail.com>
2016-09-13 11:42:24 +0100

    made some changes with sass and css compiler

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var sass = require('node-sass');
+var fs = require('fs');
 
 sass.render({
-  file: 'www/css/style.scss'
-  //[, options..]
+  file: 'www/sass/style.scss',
+  outFile: 'www/css/style.min.css',
+  outputStyle: 'compressed'
 }, function(err, result) {
-    console.log(err, result);
+  if (!err){
+    fs.writeFile('www/css/style.min.css', result.css, function(err){
+      console.log((err)? {"err": err} : {"success": "css compiled and written on disk"});
+    });
+  } else {
+    console.log({"err": err});
+  }
 });
-
-// OR
-// var result = sass.renderSync({
-//   data: scss_content
-//   [, options..]
-// });
